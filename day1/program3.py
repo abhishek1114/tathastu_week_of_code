77e10c00283c4fdfe35ce053e323bdae39f8c202
ShrutiAgarwal10 <65722587+ShrutiAgarwal10@users.noreply.github.com>
2020-05-22 17:20:04 +0530

    Create program3.py

---
+++ day1/program3.py
@@ -0,0 +1,11 @@
+n1=int(input("enter a number , a."))
+n2=int(input("enter second number, b"))
+print("numbers before swap")
+print("a is", n1)
+print("b is", n2)
+n1=n1+n2
+n2=n1-n2
+n1=n1-n2
+print("numbers after swap")
+print("a is", n1)
+print("b is", n2)

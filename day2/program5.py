eee0bdc65c6878a182ac7aa32ce0b2f4a7e49bf5
ShrutiAgarwal10 <65722587+ShrutiAgarwal10@users.noreply.github.com>
2020-05-24 16:07:48 +0530

    Create program5.py

---
+++ day2/program5.py
@@ -0,0 +1,12 @@
+for i in range(3,0,-1):
+    print(i, end="")
+    for j in range(1,i):
+        print("*", end="")
+        print(i,end="")
+    print()
+for i in range(1,4):
+    print(i, end="")
+    for j in range(1,i):
+        print("*", end="")
+        print(i,end="")
+    print()
